0eaff3edb13a59950a66c448cdedcb830968b5dd
veeskell <dvic.martins@gmail.com>
2023-05-31 16:49:29 +0100

    Update 'list less than ten'

--- practice-python/03-list-less-than-ten/list-less-than-ten.py
+++ practice-python/03-list-less-than-ten/list-less-than-ten.py
@@ -1,28 +1,17 @@
+def elems_less_than_n(l, num):
+    res = []
+    for elem in l:
+        if elem < num:
+            res.append(elem)
+    return res
+
+def elems_less_than_n_list_comprehension(l, num):
+    return [elem for elem in l if elem < num]
+
+def elems_less_than_n_filter(l, num):
+    return list(filter(lambda x: x < num, l))
+
 exercise_list = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 number = int(input("Say a number: "))
 
-l = []
-for element in exercise_list:
-    if element < number:
-        l.append(element)
-
-print(l)
-
-"""
-# Can also be written in one line using list comprehension:
-
-exercise_list = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-print([i for i in exercise_list if i < number])
-
-# Can also be done through a filter function:
-
-def is_less_than_five(x):
-  return x < 5
-
-print(list(filter(is_less_than_five, exercise_list)))
-
-# Or simplified through the usage of a lambda function:
-
-print(filter(lambda x: x < 5, exercise_list))
-
-"""
+print(elems_less_than_n_filter(exercise_list, number))
