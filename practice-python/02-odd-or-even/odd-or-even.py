cbafc37d8a51488de3893f28344ca0f5d1b1ad88
veeskell <dvic.martins@gmail.com>
2023-05-31 11:31:08 +0100

    Update 'odd or even'

--- practice-python/02-odd-or-even/odd-or-even.py
+++ practice-python/02-odd-or-even/odd-or-even.py
@@ -1,14 +1,14 @@
 num = int(input("Insert a number: "))
 check = int(input("Insert a number to divide the first number by: "))
 
-if num%4 == 0:
+if num % 4 == 0:
     print(f"{num} is a multiple of 4.", end='')
-elif num%2 == 0:
+elif num % 2 == 0:
     print(f"{num} is an even number.", end='')
 else:
     print(f"{num} is an odd number.", end='')
 
-if num%check == 0:
+if num % check == 0:
     print(f" {num} can be divded by {check}.")
 else:
     print(f" {num} can't be divded by {check}.")
